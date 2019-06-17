3fbfebb11989b842ed87cddcfe3000d0e6801c76
Volodymyr Osmak <darkxahtep@gmail.com>
2019-06-17 18:02:09 +0300

    Added hello world CLI tool

---
+++ DarkXaHTeP.CompressedStaticFiles.Tools/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DarkXaHTeP.CompressedStaticFiles.Tools
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
