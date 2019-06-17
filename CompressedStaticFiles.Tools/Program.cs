26f469f527089657919b70b1c2b612eff959101b
Volodymyr Osmak <darkxahtep@gmail.com>
2019-06-17 23:24:18 +0300

    Added initial logic and updated readme

--- CompressedStaticFiles.Tools/Program.cs
+++ CompressedStaticFiles.Tools/Program.cs
@@ -1,12 +1,58 @@
 using System;
+using System.IO;
+using System.Linq;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace DarkXaHTeP.CompressedStaticFiles.Tools
 {
-    public static class Program
+    public class Program
     {
-        public static void Main()
+        [Argument(0, "search_pattern1 search_pattern2 ...")]
+        public string[] FileMasks { get; } = null;
+
+        [Option("-w|--workdir", "Use directory relative to CWD", CommandOptionType.SingleValue)]
+        public string WorkDir { get; } = null;
+
+        [Option("-gz|--gzip", "Compress to GZip", CommandOptionType.NoValue)]
+        public bool GZipRequested { get; } = false;
+        
+        [Option("-br|--brotli", "Compress to GZip", CommandOptionType.NoValue)]
+        public bool GZipRequested { get; } = false;
+
+
+        public int OnExecute()
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("GZip: " + GZipRequested);
+            
+            var currentDirPath = Directory.GetCurrentDirectory();
+            var workDirPath = WorkDir == null ? currentDirPath : Path.Combine(currentDirPath, WorkDir);
+            
+            var workDir = new DirectoryInfo(workDirPath);
+            
+            Console.WriteLine("Working Directory: " + workDirPath);
+
+            if (!workDir.Exists)
+            {
+                Console.WriteLine("Directory doesn't exist");
+                return -1;
+            }
+
+            var files = (FileMasks ?? new []{ "*.*" }).SelectMany(m => workDir.GetFiles(m, SearchOption.AllDirectories))
+                .Select(fi => GetRelativePath(workDirPath, fi.FullName)).Distinct().ToArray();
+
+            foreach (var file in files)
+            {
+                Console.WriteLine("- " + file);
+            }
+
+            return 0;
         }
+
+        private string GetRelativePath(string workDirPath, string fullName)
+        {
+            return Path.GetRelativePath(workDirPath, fullName);
+        }
+
+        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DarkXaHTeP.CompressedStaticFiles.Tools
-A CLI tool that allows to compress static assets using Brotli, Gzip or Deflate to be served by CompressedStaticFiles middleware later
+A CLI tool that allows to compress static assets using Brotli or GZip to be served by CompressedStaticFiles middleware later
