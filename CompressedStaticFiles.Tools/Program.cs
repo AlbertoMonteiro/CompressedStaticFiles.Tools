e6a80316372681efd04fb2cc06cd85b0e6d76454
Volodymyr Osmak <darkxahtep@gmail.com>
2019-06-18 00:50:30 +0300

    Added initial working implementation

--- CompressedStaticFiles.Tools/Program.cs
+++ CompressedStaticFiles.Tools/Program.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
 
 namespace DarkXaHTeP.CompressedStaticFiles.Tools
 {
     public class Program
     {
+        private string _workDirPath;
+
         [Argument(0, "search_pattern1 search_pattern2 ...")]
         public string[] FileMasks { get; } = null;
 
@@ -19,8 +24,10 @@ namespace DarkXaHTeP.CompressedStaticFiles.Tools
         [Option("-br|--brotli", "Compress to GZip", CommandOptionType.NoValue)]
         public bool UseBrotli { get; } = false;
 
+        [Option("--batch", "Number of compressions to execute simultaneously", CommandOptionType.SingleValue)]
+        public uint BatchSize { get; } = 1;
 
-        public int OnExecute()
+        public async Task<int> OnExecute()
         {
             if (!UseBrotli && !UseGZip)
             {
@@ -29,11 +36,11 @@ namespace DarkXaHTeP.CompressedStaticFiles.Tools
             }
             
             var currentDirPath = Directory.GetCurrentDirectory();
-            var workDirPath = WorkDir == null ? currentDirPath : Path.Combine(currentDirPath, WorkDir);
+            _workDirPath = WorkDir == null ? currentDirPath : Path.Combine(currentDirPath, WorkDir);
             
-            var workDir = new DirectoryInfo(workDirPath);
+            var workDir = new DirectoryInfo(_workDirPath);
             
-            Console.WriteLine("Working Directory: " + workDirPath);
+            Console.WriteLine("Working Directory: " + _workDirPath);
 
             if (!workDir.Exists)
             {
@@ -41,15 +48,80 @@ namespace DarkXaHTeP.CompressedStaticFiles.Tools
                 return -1;
             }
 
-            var files = (FileMasks ?? new []{ "*.*" }).SelectMany(m => workDir.GetFiles(m, SearchOption.AllDirectories))
-                .Select(fi => GetRelativePath(workDirPath, fi.FullName)).Distinct().ToArray();
+            var files = (FileMasks ?? new []{ "*.*" })
+                .SelectMany(m => workDir.GetFiles(m, SearchOption.AllDirectories))
+                .ToLookup(x => x.FullName)
+                .Select(x => x.First())
+                .ToList();
 
-            foreach (var file in files)
+            var compressions = files.SelectMany(f => CreateCompressionTasks(f)).ToList();
+
+            var batchSize = (BatchSize == 0 || BatchSize > 99) ? 1 : BatchSize;
+
+            var batches = SplitCompressions(compressions, batchSize);
+
+            foreach (var batch in batches)
             {
-                Console.WriteLine("- " + file);
+                await Task.WhenAll(batch.Select(task =>
+                {
+                    var (fi, compress) = task;
+                    return compress(fi);
+                }));
             }
 
             return 0;
+        }
+
+        private IEnumerable<IEnumerable<(FileInfo, Func<FileInfo, Task>)>> SplitCompressions(IReadOnlyList<(FileInfo, Func<FileInfo,Task>)> compressions, uint batchSize)
+        {
+            int bSize = Convert.ToInt32(batchSize);;
+
+            for (var i = 0; i < (float)compressions.Count / batchSize; i++)
+            {
+                yield return compressions.Skip(i * bSize).Take(bSize);
+            }
+        }
+
+        private IEnumerable<(FileInfo, Func<FileInfo, Task>)> CreateCompressionTasks(FileInfo fi)
+        {
+            if (UseGZip)
+            {
+                yield return (fi, CreateGZip);
+            }
+
+            if (UseBrotli)
+            {
+                yield return (fi, CreateBrotli);
+            }
+        }
+
+        private async Task CreateBrotli(FileInfo fi)
+        {
+            await CreateCompressed(fi, ".br", "BROTLI", stream => new BrotliStream(stream, CompressionLevel.Optimal));
+        }
+
+        private async Task CreateGZip(FileInfo fi)
+        {
+            await CreateCompressed(fi, ".gz", "GZIP", stream => new GZipStream(stream, CompressionLevel.Optimal));
+        }
+
+        private async Task CreateCompressed(FileInfo fi, string extension, string name, Func<Stream, Stream> createCompressStream)
+        {
+            var destFileInfo = new FileInfo(fi.FullName + extension);
+
+            using (var readStream = fi.Open(FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                Console.WriteLine($"[{name}][Open] {GetRelativePath(_workDirPath, fi.FullName)} [{fi.Length / 1024}KB]");
+                using (var writeStream = destFileInfo.Open(FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    using (var brStream = createCompressStream(writeStream))
+                    {
+                        await readStream.CopyToAsync(brStream);                        
+                    }
+                }
+            }
+
+            Console.WriteLine($"[{name}][Done] {GetRelativePath(_workDirPath, destFileInfo.FullName)} [{destFileInfo.Length / 1024}KB]");
         }
 
         private string GetRelativePath(string workDirPath, string fullName)
